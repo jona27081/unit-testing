275edeae32aab4c32177a67677142f7166f32704
jona2708 <104945335+jona27081@users.noreply.github.com>
2022-10-04 01:16:22 -0500

    add pow y prueba of pow #5 (#7)

---
+++ src/app/03-pow/pow.spec.ts
@@ -0,0 +1,26 @@
+import { pow } from './pow'
+
+describe('pow test suite', () => {
+    it('Should return 1 if n is 0', () => {
+        const result = pow(0);
+        expect(result).toBe(1);
+    })
+
+    it('Should return 0 if n is negative', () => {
+        const result =pow(-1);
+        expect(result).toBe(0);
+    })
+
+    it('Should return 8 if x=2, n=3', () => {
+        const result =pow(2, 3);
+        expect(result).toBe(8);
+    })
+
+    it('Should return 9 if x=3, n=2', () => {
+        const result =pow(3, 2);
+        expect(result).toBe(9);
+    })
+
+
+
+})
